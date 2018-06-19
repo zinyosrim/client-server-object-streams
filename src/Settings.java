561510508e93404b0df0829112b80c884e0186ad
Zin Yosrim <zinyosrim@gmail.com>
2018-06-19 17:26:05 +0200

    Add Settings.java

---
+++ src/Settings.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * Settings and Configuration Class for storing constants.
+ * Usage example: {@code int portId = Settings.PORT;}
+ */
+public class Settings {
+
+    // Constants
+    public static final int PORT = 4321;
+}
